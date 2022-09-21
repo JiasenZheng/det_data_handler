2d1e9fbec3dff5d9e7d6c8a3c9fa04d08a19734e
Jiasen Zheng <jiasenzheng2020@u.northwestern.edu>
2022-09-21 11:12:08 -0500

    create an optical flow launch file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['det_data_handler'],
     package_dir={'': 'python'},
-    scripts=['scripts/det_data_handler_node.py'],
+    scripts=['scripts/det_data_handler_node'],
 )
 
 setup(**d)
